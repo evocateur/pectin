a2674ca874eef3a3b613ce2811fb0902f813c026
Daniel Stockman <daniel.stockman@gmail.com>
2018-08-31 15:20:50 -0700

    chore: add tabWidth overrides for yaml + json config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,21 @@
 'use strict';
 
+const config = require('eslint-plugin-zillow/prettier.config');
+
 // just here so the editor plugins don't get fired
-module.exports = require('eslint-plugin-zillow/prettier.config');
+module.exports = Object.assign({}, config, {
+    overrides: [
+        {
+            files: [
+                'lerna.json',
+                'package.json',
+                'package-lock.json',
+                '.eslintrc.yaml',
+                '.travis.yml',
+            ],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+});
