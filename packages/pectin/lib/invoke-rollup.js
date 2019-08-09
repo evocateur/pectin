dca84fc5c2bb65da5e0616ae847d12547dcfb1ef
Daniel Stockman <daniel.stockman@gmail.com>
2019-08-09 12:29:20 -0700

    fix(cli): Resolve rollup bin from package metadata instead of hard-coded subpath
    The Rollup executable location was changed in v1.19.0, which broke our fancy hacks

--- packages/pectin/lib/invoke-rollup.js
+++ packages/pectin/lib/invoke-rollup.js
@@ -15,10 +15,12 @@ function invokeRollup(argv) {
         opts.unshift('--watch');
     }
 
+    /* eslint-disable global-require, zillow/import/no-dynamic-require */
     // @see https://github.com/zkat/npx/blob/b7c8b9f07605b9f41931ad3ef8e74a65d2f062bb/index.js#L258-L268
-    const Module = require('module'); // eslint-disable-line global-require
+    const Module = require('module');
     const rollupPkg = resolveFrom(argv.cwd, 'rollup/package.json');
-    const rollupBin = path.join(path.dirname(rollupPkg), 'bin/rollup');
+    // instead of hard-coded subpath, read package.json metadata to retrieve bin location
+    const rollupBin = path.join(path.dirname(rollupPkg), require(rollupPkg).bin.rollup);
 
     process.argv = [process.argv[0], rollupBin].concat(opts);
 
