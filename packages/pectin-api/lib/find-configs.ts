f4d0767eb8bdafff4a72f0492d36660534c44d71
Daniel Stockman <daniel.stockman@gmail.com>
2019-11-11 17:49:34 -0800

    refactor(typescript): Add return type for findConfigs()

--- packages/pectin-api/lib/find-configs.ts
+++ packages/pectin-api/lib/find-configs.ts
@@ -3,14 +3,20 @@ import project = require('@lerna/project');
 import runTopologically = require('@lerna/run-topologically');
 
 import { CoreProperties as PackageManifest } from '@schemastore/package';
+import { RollupOptions } from 'rollup';
 
 import { generateConfig } from './generate-config';
 
-export async function findConfigs(opts: { cwd?: string; concurrency: number; watch: boolean }) {
-    const concurrency = opts.concurrency || os.cpus().length;
-    const watch = opts.watch || !!process.env.ROLLUP_WATCH;
+export async function findConfigs(opts?: {
+    cwd?: string;
+    concurrency: number;
+    watch: boolean;
+}): Promise<RollupOptions[]> {
+    const concurrency = (opts && opts.concurrency) || os.cpus().length;
+    const watch =
+        opts && typeof opts.watch !== 'undefined' ? opts.watch : !!process.env.ROLLUP_WATCH;
 
-    const lernaPackages = await project.getPackages(opts.cwd);
+    const lernaPackages = await project.getPackages((opts && opts.cwd) || process.cwd());
     const configs = await runTopologically(
         lernaPackages,
         // clones internal JSON, maps synthetic location to cwd property
